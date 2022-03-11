8a666907b4dc40d26fedf0ab95c05bfff3316e83
Parveen Gupta <pg@macbook-pro.lan>
2022-03-10 18:19:54 -0700

    added sample code

--- src/main/java/com/example/sample/CallerSample.java
+++ src/main/java/com/example/sample/CallerSample.java
@@ -1,5 +1,18 @@
 package com.example.sample;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+
 public class CallerSample {
 
     public String getToken(){
@@ -10,4 +23,37 @@ public class CallerSample {
         String token = tokenGenerator.generateToken("client", "otherParam");
         return token;
     }
+
+    public static void main(String[] args) {
+        getToken_v2();
+    }
+    public static String getToken_v2(){
+        String certPath = "/Users/pg/work/projects/java/jwt-cert/TestServer.pem";
+        String keyPath = "/Users/pg/work/projects/java/jwt-cert/TestServer.key";
+
+        X509Certificate cer = null;
+        PrivateKey privateKey = null;
+        try {
+            CertificateFactory fact = CertificateFactory.getInstance("X.509");
+            FileInputStream is = new FileInputStream(certPath);
+            cer = (X509Certificate) fact.generateCertificate(is);
+            PublicKey key = cer.getPublicKey();
+            is.close();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        try {
+            Path path = Paths.get(keyPath);
+            byte[] privKeyByteArray = Files.readAllBytes(path);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privKeyByteArray);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            privateKey= keyFactory.generatePrivate(keySpec);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        IDTokenGenerator tokenGenerator = new IDTokenGenerator(privateKey,cer);
+        String token = tokenGenerator.generateToken("client", "otherParam");
+        return token;
+    }
+
 }
